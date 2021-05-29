d0e65c86facda41c3c74d875787a2778e8fe883a
James Salvatore <jimmy.c.salvatore@gmail.com>
2021-05-28 21:04:36 -0500

    chore: updated win removes;

--- OS/windows/init_windows_liteweight.sh
+++ OS/windows/init_windows_liteweight.sh
@@ -2,15 +2,34 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # ---
-# DISM /Online /Get-ProvisionedAppxPackages | select-string Packagename 
-# $excludedApps = ‘.*photos.*|.*sticky.*|.*zunevideo.*’
 
-# $unwantedApps = Get-AppxPackage -PackageTypeFilter Bundle | Where-Object {$_.Name -notmatch $excludedApps}
-
-# If ($unwantedApps) {
-
-# $unwantedApps | Remove-AppxPackage
-# }
+Get-AppxPackage *BingWeather* | Remove-AppxPackage
+Get-AppxPackage *GetHelp* | Remove-AppxPackage
+Get-AppxPackage *Getstarted* | Remove-AppxPackage
+Get-AppxPackage *Messaging* | Remove-AppxPackage
+Get-AppxPackage *Microsoft3DViewer* | Remove-AppxPackage
+Get-AppxPackage *MicrosoftOfficeHub* | Remove-AppxPackage
+Get-AppxPackage *MicrosoftSolitaireCollection* | Remove-AppxPackage
+Get-AppxPackage *MicrosoftStickyNotes* | Remove-AppxPackage
+Get-AppxPackage *MixedReality.Portal* | Remove-AppxPackage
+Get-AppxPackage *Office.OneNote* | Remove-AppxPackage
+Get-AppxPackage *OneConnect* | Remove-AppxPackage
+Get-AppxPackage *Print3D* | Remove-AppxPackage
+Get-AppxPackage *SkypeApp* | Remove-AppxPackage
+Get-AppxPackage *Wallet* | Remove-AppxPackage
+Get-AppxPackage *WindowsAlarms* | Remove-AppxPackage
+Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+Get-AppxPackage *WindowsFeedbackHub* | Remove-AppxPackage
+Get-AppxPackage *WindowsMaps* | Remove-AppxPackage
+Get-AppxPackage *WindowsSoundRecorder* | Remove-AppxPackage
+Get-AppxPackage *XboxApp* | Remove-AppxPackage
+Get-AppxPackage *XboxGameOverlay* | Remove-AppxPackage
+Get-AppxPackage *XboxGamingOverlay* | Remove-AppxPackage
+Get-AppxPackage *XboxIdentityProvider* | Remove-AppxPackage
+Get-AppxPackage *XboxSpeechToTextOverlay* | Remove-AppxPackage
+Get-AppxPackage *YourPhone* | Remove-AppxPackage
+Get-AppxPackage *ZuneMusic* | Remove-AppxPackage
+Get-AppxPackage *ZuneVideo* | Remove-AppxPackage
 
 # ---
 # SEPARATE INSTALL: notion, twilio, huion-tablet screen-to-gif
