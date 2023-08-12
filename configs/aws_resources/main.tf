d88373f7690933a903c20623f1db6402409ca53d
James Salvatore <jimmy.c.salvatore@gmail.com>
2023-08-12 09:55:05 -0500

    chore: removes leafroom;

--- configs/aws_resources/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "james-salvatore-leaf-room-podcast"
-}
-
-resource "aws_s3_bucket_acl" "acl" {
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "private"
-}
